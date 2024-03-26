4cc3206d3bb6dbd609fb07a2a134d23658f2d0e4
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-03-26 22:41:05 +0100

    Done with the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import chatLogo from "../src/assets/chat-logo.png";
+import sendButton from "../src/assets/send.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <section className="side-bar">
-        <button className="new-chat"> + New Chat</button>
+        <div className="new-chat">
+          <img src={chatLogo} className="chat-logo" />
+          <p>New Chat</p>
+        </div>
         <ul className="history"></ul>
         <nav>
           <p>made by Alex</p>
@@ -14,15 +19,15 @@ function App() {
       <section className="main">
         <h1>Alexgpt</h1>
         <ul className="feed"></ul>
-        <div className="bottom-section"></div>
-        <div className="input-container">
-          <input type="text" />
-          <div id="submit">&gt;</div>
-        </div>
-        <div className="info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni minima
-          est blanditiis nam eum? Recusandae eos delectus excepturi nesciunt,
-          saepe temporibus doloribus quam fugit quisquam!
+        <div className="bottom-section">
+          <div className="input-container">
+            <input type="text" placeholder="Message Alexgpt" />
+            <img src={sendButton} class="submit" />
+          </div>
+          <div className="info">
+            Alexgpt can make mistakes. We're improving and just trying to make
+            your life easier and better
+          </div>
         </div>
       </section>
     </div>
